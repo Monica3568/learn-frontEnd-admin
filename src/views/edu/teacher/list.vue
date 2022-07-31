aa6747462ca02630789f6e467130105324f9912a
Monica <305064132@qq.com>
2022-07-31 23:27:49 +0800

    23点27分 提交

--- src/views/edu/teacher/list.vue
+++ src/views/edu/teacher/list.vue
@@ -1,11 +1,61 @@
 <template>
-  <div class="app-container" />
-</template>
+  <div class="app-container">
+    <!-- 表格 -->
 
+    <el-table
+      :data="list"
+      element-loading-text="数据加载中"
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column label="序号" width="70" align="center">
+        <template slot-scope="scope">
+          {{ (page - 1) * limit + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="name" label="名称" width="80" />
+      <el-table-column label="头衔" width="80">
+        <template slot-scope="scope">
+          {{ scope.row.level === 1 ? '高级讲师' : '首席讲师' }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="intro" label="资历" />
+      <el-table-column prop="gmtCreate" label="添加时间" width="160" />
+      <el-table-column prop="sort" label="排序" width="60" />
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <router-link :to="'/edu/teacher/edit/' + scope.row.id">
+            <el-button type="primary" size="mini" icon="el-icon-edit"
+              >修改</el-button
+            >
+          </router-link>
+          <el-button
+            type="danger"
+            size="mini"
+            icon="el-icon-delete"
+            @click="removeDataById(scope.row.id)"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    分页
+    <el-pagination
+      :current-page="page"
+      :page-size="limit"
+      :total="total"
+      style="padding: 30px 0; text-align: center"
+      layout="total, prev, pager, next, jumper"
+      @current-change="getList"
+    />
+  </div>
+</template>
 <script>
 import teacher from '@/api/edu/teacher'
 export default {
-  data() { // 定义变量和初始值
+  data() {
+    // 定义变量和初始值
     return {
       list: null,
       total: 0,
@@ -14,16 +64,23 @@ export default {
       teacherQuery: {}
     }
   },
-  created() { // 页面渲染之前执行，一般调用methods定义的方法
+  created() {
+    // 页面渲染之前执行，一般调用methods定义的方法
     this.getList()
   },
-  methods: { // 创建具体的方法，掉用teacher.js的方法
-    getList() {
-      teacher.getTeacherListPage(this.page, this.limit, this.teacherQuery)
-        .then(response => {
-          console.log(response)
+  methods: {
+    // 创建具体的方法，掉用teacher.js的方法
+    getList(page = 1) {
+      this.page = page
+      teacher
+        .getTeacherListPage(this.page, this.limit, this.teacherQuery)
+        .then((response) => {
+          this.list = response.data.rows
+          this.total = response.data.total
+          console.log(this.list)
+          console.log(this.total)
         }) // 请求成功
-        .catch(error => {
+        .catch((error) => {
           console.log(error)
         }) // 请求失败
     }
